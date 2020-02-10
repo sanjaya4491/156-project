b945f55e04a5354bc857fa4fc5ac14f72c69d9c1
dgodoy2 <54423370+dgodoy2@users.noreply.github.com>
2020-02-09 19:07:15 -0600

    WHATEVR

--- Project156/src/com/tbf/main.java
+++ Project156/src/com/tbf/main.java
@@ -1,17 +1,25 @@
 package com.tbf;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class main {
 	
-	
+	public static void main(String args[]) {
+		
 	List <Person> personList = new ArrayList <Person>();
 	File file = new File ("data/Persons.dat");
-	Scanner sc = new Scanner(file);
+	Scanner sc = null;
+	try {
+		sc = new Scanner(file);
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 
 
 	
-
+	}
 }
