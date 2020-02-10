56099532739d4faa26c64a3e6d43e9b26532a4bd
sanjaya4491 <54679398+sanjaya4491@users.noreply.github.com>
2020-02-09 19:03:47 -0600

    main

--- Project156/src/com/tbf/DataConverter.java
+++ Project156/src/com/tbf/DataConverter.java
@@ -1,5 +1,6 @@
 package com.tbf;
 
 public class DataConverter {
+	
 
 }
---
+++ Project156/src/com/tbf/main.java
@@ -0,0 +1,17 @@
+package com.tbf;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class main {
+	
+	
+	List <Person> personList = new ArrayList <Person>();
+	File file = new File ("data/Persons.dat");
+	Scanner sc = new Scanner(file);
+
+
+	
+
+}
