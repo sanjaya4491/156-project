b4574c3ca4b999af017a5f5d1096117dffd8e9ff
dgodoy2 <54423370+dgodoy2@users.noreply.github.com>
2020-02-09 18:21:46 -0600

    Email class

--- Project156/src/com/tbf/Email.java
+++ Project156/src/com/tbf/Email.java
@@ -1,5 +1,19 @@
 package com.tbf;
 
 public class Email {
+	
+	private String email;
+
+	public Email(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
