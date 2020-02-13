7ce1998fbcd20ebdd0e038b2d214f0b17943cf64
dgodoy2 <54423370+dgodoy2@users.noreply.github.com>
2020-02-13 14:09:27 -0600

    File utils and data stuff change

--- Project156/src/com/tbf/DataConverter.java
+++ Project156/src/com/tbf/DataConverter.java
@@ -1,41 +1,12 @@
 package com.tbf;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.lang.reflect.Type;
-import java.util.List;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-
 public class DataConverter {
 	
 	public static void main(String args[]) {
 		
-		List<Person> personList = FileUtils.parsePersonDataFile();
-		List<Asset> assetList = FileUtils.parseAssetDataFile();
+		FileUtils.personListToJsonFile();
+		FileUtils.assetListToJsonFile();
 		
-		PrintWriter outPerson = null;
-		PrintWriter outAsset = null;
-		try {
-			outPerson = new PrintWriter("data/Persons.json");
-			outAsset = new PrintWriter("data/Assets.json");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		
-		String json = "{" + "\n" + "\"persons\": " + gson.toJson(personList)  + "}";
-		outPerson.write(json);
-		
-		String json2 = "{" + "\n" + "\"assets\": " + gson.toJson(assetList) + " }";
-		outAsset.write(json2);
-		
-		outPerson.close();
-		outAsset.close();
 	}
 	
-
 }
--- Project156/src/com/tbf/FileUtils.java
+++ Project156/src/com/tbf/FileUtils.java
@@ -2,9 +2,13 @@ package com.tbf;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class FileUtils {
 
@@ -96,6 +100,36 @@ public class FileUtils {
 		}
 		sc.close();
 		return result;
+	}
+	
+	public static void personListToJsonFile() {
+		List<Person> personList = FileUtils.parsePersonDataFile();
+		PrintWriter out = null;
+		try {
+			out = new PrintWriter("data/Persons.json");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = "{" + "\n" + "\"persons\": " + gson.toJson(personList)  + "}";
+		out.write(json);
+		out.close();
+	}
+	
+	public static void assetListToJsonFile() {
+		List<Asset> assetList = FileUtils.parseAssetDataFile();
+		PrintWriter out = null;
+		try {
+			out = new PrintWriter("data/Assets.json");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = "{" + "\n" + "\"assets\": " + gson.toJson(assetList) + "}";
+		out.write(json);
+		out.close();
 	}
 
 }
