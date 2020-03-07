eb8a1f566fe10893f2aa39bfc2dcda9d83285da3
dgodoy2 <54423370+dgodoy2@users.noreply.github.com>
2020-03-06 20:18:47 -0600

    :))

--- Project156/src/com/tbf/PortfolioUtils.java
+++ Project156/src/com/tbf/PortfolioUtils.java
@@ -40,18 +40,18 @@ public class PortfolioUtils {
 			returnTotal += portfolio.getTotalRateOfReturn();
 			totalTotal += portfolio.getTotalPortfolioValue();
 
-			System.out.printf("%-10s %-20s %-20s %-5s %-10.2f %-5s %-13.2f %-13.4f %-5s %-10.2f %-5s %-10.2f\n",
+			System.out.printf(String.format("%-10s %-20s %-20s %-5s %-10.2f %-5s %-13.2f %-13.4f %-5s %-10.2f %-5s %-10.2f\n",
 					portfolio.getPortfolioCode(), portfolio.getOwner().toString(), portfolio.getManager().toString(), "$", portfolio.getManager().getFee() * portfolioAssetListSize, "$",
 					portfolio.getManager().getCommission() * portfolio.getTotalRateOfReturn(), portfolio.getWeightedRisk(), "$",
 					portfolio.getTotalRateOfReturn(), "$", portfolio.getTotalPortfolioValue(), "$",
-					portfolio.getWeightedRisk(), "$", portfolio.getTotalRateOfReturn());
+					portfolio.getWeightedRisk(), "$", portfolio.getTotalRateOfReturn()));
 		}
 
 		System.out.println(
 				"                                                     ========================================================================================");
 
-		System.out.printf(" %50s  $ %10.2f     $ %10.2f   %20s %12.2f   $ %12.2f\n\n\n", "Total", feesTotal, commissionsTotal,
-				"$", returnTotal, totalTotal);
+		System.out.printf(String.format(" %50s  $ %10.2f     $ %10.2f   %20s %12.2f   $ %12.2f\n\n\n", "Total", feesTotal, commissionsTotal,
+				"$", returnTotal, totalTotal));
 	}
 	
 	/**
