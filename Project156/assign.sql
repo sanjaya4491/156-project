c257109965ff62491aa6ec0d4084efc21f16a540
dgodoy2 <54423370+dgodoy2@users.noreply.github.com>
2020-03-09 12:21:38 -0500

    Update assign.sql
    updated assign.sql

--- Project156/assign.sql
+++ Project156/assign.sql
@@ -9,8 +9,6 @@ drop table if exists Address;
 drop table if exists State;
 drop table if exists Country;
 
-
-
 create table Country (
 countryId int not null primary key auto_increment,
 country varchar (255) not null
@@ -18,7 +16,7 @@ country varchar (255) not null
 
 create table State(
 stateId int not null primary key auto_increment,
-state varchar (255),
+state varchar (255) not null,
 countryId int not null,
 foreign key (countryId) references Country(countryId)
 );
@@ -49,32 +47,26 @@ personId int not null,
 foreign key (personId) references Person (personId)
 );
 
-
-
-
-
 create table Asset(
 assetId int not null primary key auto_increment,
+assetCode varchar (255),
+assetType varchar(255),
 assetName varchar(255) not null,
-apr double ,
-quartlydividend double,
+apr double,
+quarterlydividend double,
 baseReturn double,
 omegaMeasure double,
 totalValue double,
 stockSymbol varchar(255),
-sharePrice double,
-assetType varchar(255),
-assetCode varchar (255)
+sharePrice double
 );
-
-
 
 create table Portfolio(
 portfolioId int not null primary key auto_increment,
+portfolioCode varchar(255) not null,
 personId int not null,
 brokerid int null,
 beneficiaryId int ,
-portfolioCode varchar(255) not null,
 foreign key (personId) references Person (personId)
 );
 
@@ -87,8 +79,6 @@ foreign key (portfolioId) references Portfolio (portfolioId),
 foreign key (assetId) references Asset (assetId)
 );
 
-show tables;
-
 insert into Country (country)values
 ("USA");
 
@@ -97,21 +87,19 @@ insert into State (state , countryId) values
 ("IL", (select countryId from Country where country = "USA")),
 ("NY", (select countryId from Country where country = "USA"));
 
-insert into Address (street, city, zipCode, stateId)values
+insert into Address (street, city, zipCode, stateId) values
 ("9903 Jenifer Streets", "Fremont", 68025, (select stateId from State where state = "NE")),
 ("1 School Road" , "Chicago", 60613, (select stateId from State where state = "IL")),
 ("8 Ronald Regan Hill" , "Omaha", 68116, (select stateId from State where state = "NE")),
 ("1 Independence Plaza" , "New York", 10004, (select stateId from State where state = "NY")),
 ("64516 Golf View Drive" , "Omaha", 68116, (select stateId from State where state = "NE" ));
 
-
-insert into Person (firstName, lastName, personCode, brokerType, addressId)values
+insert into Person (firstName, lastName, personCode, brokerType, addressId) values
 ("Salvatore" , "Cordova", "007Y", "E" , (select addressId from Address where street = "9903 Jenifer Streets")),
 ("Adora" , "Sickling", "UIMG", null , (select addressId from Address where street = "1 School Road")),
 ("Dionis" , "Handscomb", "YMCA", null , (select addressId from Address where street = "8 Ronald Regan Hill")),
 ("Lillian" , "De Angelis", "PK9B", "J" , (select addressId from Address where street = "1 Independence Plaza")),
 ("Quinn" , "Carress", "R555RD", null , (select addressId from Address where street = "64516 Golf View Drive"));
-
 
 insert into Email (email, personId) values
 ("cordova0@zdnet.com" , (select personId from Person where personCode = "007Y")), 
@@ -121,32 +109,27 @@ insert into Email (email, personId) values
 ("qcarress4@toplist.cz" , (select personId from Person where personCode = "R555RD")), 
 ("whatever@whatever.com" , (select personId from Person where personCode = "R555RD"));
 
-
-
 insert into Portfolio (personId, brokerId, beneficiaryId, portfolioCode ) values
-((select personId from Person where personCode = "YMCA"), (select personId from Person where personCode = "007Y"), 
-	(select personId from Person where personCode = "UIMG"), "PT001"),
+((select personId from Person where personCode = "YMCA"), (select personId from Person where personCode = "007Y"), (select personId from Person where personCode = "UIMG"), "PT001"),
 ((select personId from Person where personCode = "UIMG"), (select personId from Person where personCode = "PK9B"),null, "PT002"),
 ((select personId from Person where personCode = "R555RD"), (select personId from Person where personCode = "PK9B"),null, "PT003");
 
+select * from Person;
 select * from Portfolio;
 
-
-insert into Asset (assetName, assetType, assetCode, apr, quartlydividend ,
+insert into Asset (assetName, assetType, assetCode, apr, quarterlydividend ,
 					baseReturn ,omegaMeasure ,totalValue ,stockSymbol ,sharePrice ) values
-                    
-		("Money Market Savings", "D", "BX001-23", 13.05, null, null, null, null, null, null),
-		("Savings Account", "D", "CCBB00", 0.24, null, null, null, null, null, null),
-		("4-year CD", "D", "99800", 4.35, null, null, null, null, null, null),
-		("3-year Rollover CD", "D", "apple", 2.35, null, null, null, null, null, null),
-		("2-year Rollover CD", "D", "dell22", 2.75, null, null, null, null, null, null),		
-        ("chrome Inc.", "S", "google", null, 0.0, 5.6, -0.05, null, "alpabte", 84.708),
-		("Diageo PLC", "S", "dadooe90", null, 12.00, 3.2, 0.01, null, "DEO", 12.75),
-		("acccv", "S", "324yoo", null, 24.50, 4.3, -0.07, null, "abc", 42.80),
-		("Many Midget Manufacturers Man.", "P", "AME21", null, 1000, 2 , -0.15, 4333, null, null);
+("Money Market Savings", "D", "BX001-23", 13.05, null, null, null, null, null, null),
+("Savings Account", "D", "CCBB00", 0.24, null, null, null, null, null, null),
+("4-year CD", "D", "99800", 4.35, null, null, null, null, null, null),
+("3-year Rollover CD", "D", "apple", 2.35, null, null, null, null, null, null),
+("2-year Rollover CD", "D", "dell22", 2.75, null, null, null, null, null, null),		
+("chrome Inc.", "S", "google", null, 0.0, 5.6, -0.05, null, "alpabte", 84.708),
+("Diageo PLC", "S", "dadooe90", null, 12.00, 3.2, 0.01, null, "DEO", 12.75),
+("acccv", "S", "324yoo", null, 24.50, 4.3, -0.07, null, "abc", 42.80),
+("Many Midget Manufacturers Man.", "P", "AME21", null, 1000, 2 , -0.15, 4333, null, null);
         
-        
-        select * from Asset;
+select * from Asset;
 
 insert PortfolioAsset (portfolioId, assetId, value) values
 ((select portfolioId from Portfolio where portfolioCode = "PT001"), 
@@ -170,8 +153,8 @@ insert PortfolioAsset (portfolioId, assetId, value) values
 ((select portfolioId from Portfolio where portfolioCode = "PT003"), 
 	(select assetId from Asset where assetCode = "AME21"), 44);
 
-    
-    select * from PortfolioAsset;
+select * from PortfolioAsset;
+
 
 
 
